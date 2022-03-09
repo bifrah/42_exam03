6470a17bdea1d960b414a44244ae0257c1614aed
Benjamin <benj.ifrah@gmail.com>
2022-03-09 17:08:09 +0100

    ajout exemple

---
+++ gnl_exemple/get_next_line.c
@@ -0,0 +1,33 @@
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	stock[10000];
+	char	buf[1];
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stock[i] = '\0';
+	while (read(fd, buf, 1) == 1)
+	{
+		stock[i] = buf[0];
+		stock[i + 1] = '\0';
+		if (stock[i] == '\n')
+			break ;
+		i++;
+	}
+	line = malloc(i + 2);
+	i = 0;
+	while (stock[i])
+	{
+		line[i] = stock[i];
+		i++;
+	}
+	line[i] = '\0';
+	if (line[0] == '\0')
+		return (NULL);
+	return (line);
+}
