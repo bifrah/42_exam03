cb684a99ad606f892a6f5e5a0e07b1995a72930e
Benjamin <benj.ifrah@gmail.com>
2022-03-09 21:23:06 +0100

    add 2nd training and modif toto

---
+++ gnl_by_me/get_next_line1.c
@@ -0,0 +1,34 @@
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	stock[10000];
+	char	buf[1];
+	char	*line;
+	int	i;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	i = 0;
+	stock[0] = '\0';
+	while (read(fd, buf, 1) == 1)
+	{
+		stock[i] = buf[0];
+		stock[i + 1] = '\0';
+		if (stock[i] == '\n')
+			break ;
+		i++;
+	}
+	line = malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (stock[i])
+	{
+		line[i] = stock[i];
+		i++;
+	}
+	line[i] = '\0';
+	if (stock[0] == '\0')
+		return (NULL);
+	return (line);
+}
+
